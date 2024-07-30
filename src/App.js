74ce920e01b239eb14aadfe699ea6dadc15f3fee
german.munshau <g.munshau@yandex.ru>
2024-07-30 13:06:37 +0600

    11

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import {useEffect} from "react";
 import Header from "./components/Header/Header";
 import {useTelegram} from "./hooks/useTelegram";
+import {Route, Routes} from "react-router-dom";
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 
 function App() {
@@ -14,8 +17,13 @@ function App() {
 
     return (
         <div className="App">
-            <button onClick={onToggle}>Toggle</button>
+            {/*<button onClick={onToggle}>Toggle</button>*/}
             <Header/>
+            <Routes>
+                <Route index element={<ProductList/>}/>
+                <Route path={'/form'} element={<Form/>}/>
+            </Routes>
+
         </div>
     );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,10 +10,10 @@ const Header = () => {
         <div className={'header'}>
             <Button onClick={onClose}>Закрыть окно</Button>
             <div className={'username_info'}>
-                <span className={'username'}>first_name: {user?.first_name}</span>
-                <span className={'username'}>last_name: {user?.last_name}</span>
-                <span className={'username'}>username: {user?.username}</span>
-                <span className={'username'}>id: {user?.id}</span>
+                <span className={'username'}>User: {user?.first_name} {user?.last_name}</span>
+                {/*<span className={'username'}>last_name: {user?.last_name}</span>*/}
+                {/*<span className={'username'}>username: {user?.username}</span>*/}
+                {/*<span className={'username'}>id: {user?.id}</span>*/}
             </div>
         </div>
     );
