dfdfc720db3a6584d357362361abcf909119560c
german.munshau <g.munshau@yandex.ru>
2024-07-30 15:15:56 +0600

    11

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
+import ProductItem from "../ProductItem/ProductItem";
 import './ProductList.css'
+import {useTelegram} from "../../hooks/useTelegram";
 
+
+const products = [
+    {id: 1, title: 'Джинсы', price: 100, description: 'Синий цвет, прямая модель'},
+    {id: 2, title: 'Куртка2', price: 2000, description: 'Синий цвет, прямая модель'},
+    {id: 3, title: 'Джинсы2', price: 300, description: 'Зеленый цвет, прямая модель'},
+    {id: 4, title: 'Куртка3', price: 4000, description: 'Синий цвет, прямая модель'},
+    {id: 5, title: 'Джинсы10', price: 10000, description: 'Серый цвет, прямая модель'},
+    {id: 6, title: 'Куртка6', price: 50, description: 'Леопардовый цвет, прямая модель'},
+]
+
+const getTotalPrice = (items = []) => {
+    return items.reduce((acc, item) => {
+        return acc += item.price
+    }, 0)
+}
 const ProductList = () => {
+
+    const [addedItems, setAddedItems] = useState([])
+    const {tg} = useTelegram()
+    const onAdd = (product) => {
+        const alreadyAdded = addedItems.find(item => item.id === product.id)
+        let newItems = []
+
+        if (alreadyAdded) {
+            newItems = addedItems.filter(item => item.id !== product.id)
+        } else {
+            newItems = [...addedItems, product]
+        }
+        setAddedItems(newItems)
+
+        if (newItems.length === 0) {
+            tg.MainButton.hide()
+        } else {
+            tg.MainButton.show()
+            tg.MainButton.setParams({
+                text: `Купить ${getTotalPrice(newItems)}`
+            })
+        }
+    }
+
     return (
-        <div>
-ProductList
+        <div className={'list'}>
+            {products.map(item => (
+                <ProductItem product={item}
+                             onAdd={onAdd}
+                             className={'item'}/>
+            ))}
         </div>
     );
 };
